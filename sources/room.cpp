2891453ed6962b33a247cc8b7bc713679de4994e
florian-d-agostino <florian.d-agostino@laplateforme.io>
2026-05-19 09:39:53 +0200

    feat/room [Checkpoint] - FD

---
+++ sources/room.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Room{
+
+    string name;
+    int antmax;
+    int ant;
+    list <Room> exits;
+    bool isfull ;
+
+    addDirection(room){
+        
+    }
+
+
+
+}
